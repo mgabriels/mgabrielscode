35bc9625fb5cefe353c8011fb69c163660ae2ed8
mikaela <mgabriels@my365-uwc.ac.za>
2020-09-09 23:08:10 +0200

    testing

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uwclearningdotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {string myString = "You Got This";
+        Console.WriteLine(myString);
+
+
+            
+        }
+    }
+}
